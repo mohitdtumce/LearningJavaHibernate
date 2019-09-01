b8f2e19b8408a492a981fe96f5bb8c88f435b60d
mohitsharma-r <mohitsharma.r@flipkart.com>
2019-09-02 04:05:44 +0530

    Removed un-necessary JARs

--- modules/user/src/main/java/com/herokuapp/mohitdtumce/QuickSnapMain.java
+++ modules/user/src/main/java/com/herokuapp/mohitdtumce/QuickSnapMain.java
@@ -6,7 +6,7 @@ import com.herokuapp.mohitdtumce.utils.CommonDatabaseUtils;
 public class QuickSnapMain {
 
 	public static void main(String[] args) {
-		CommonDatabaseUtils.addUserCredentials("mohitdtumce", "mohitdtumce@gmail.com", "Pass@123");
+		CommonDatabaseUtils.addUserCredentials("mohitdtum", "mohitdtum@gmail.com", "Pass@123");
 		UserCredentials userCredentials = CommonDatabaseUtils.fetchUserCredentials("mohitdtumce");
 		System.out.println(userCredentials.getUser() + userCredentials.getEmail() + userCredentials.getPassword());
 	}
